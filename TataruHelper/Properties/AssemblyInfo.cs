8125df2979d86b602bebf1efce7f893429340026
Yaroslav <prograneo@gmail.com>
2026-06-02 23:36:51 +0800

    chore: Increace app version

--- TataruHelper/Properties/AssemblyInfo.cs
+++ TataruHelper/Properties/AssemblyInfo.cs
@@ -54,6 +54,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.8")]
-[assembly: AssemblyFileVersion("0.10.8")]
+[assembly: AssemblyVersion("0.10.9")]
+[assembly: AssemblyFileVersion("0.10.9")]
 [assembly: InternalsVisibleTo("TataruHelper.Tests")]
